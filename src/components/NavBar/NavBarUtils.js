477fa1ef1cf2331eb4be513193fa170d6468f130
hobyfrezk <hobyfrezk@gmail.com>
2022-09-18 23:56:23 -0400

    Chore: remove un-needed onClick

--- src/components/NavBar/NavBarUtils.js
+++ src/components/NavBar/NavBarUtils.js
@@ -48,7 +48,7 @@ export const MyListItem = (props) => {
 	);
 
 	return (
-		<ListItem button component={CustomLink} className={classes.listItem} onClick={()=>props.onClick()}>
+		<ListItem button component={CustomLink} className={classes.listItem} >
 			{props.icon ? props.icon : null}
 			<ListItemText primary={primary} disableTypography />
 		</ListItem>
